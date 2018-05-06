d90d074e726e5603acdc55299ac0fd1dc7b6f680
Hailan Xu <hailan@ualberta.ca>
2018-05-05 22:49:51 -0600

    Can now close 'about' page

--- SafeWord/app/src/main/java/com/example/safeword/safeword/AboutActivity.java
+++ SafeWord/app/src/main/java/com/example/safeword/safeword/AboutActivity.java
@@ -2,14 +2,25 @@ package com.example.safeword.safeword;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
+    private ImageView close;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        
+
+        close = findViewById(R.id.closeButton);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
