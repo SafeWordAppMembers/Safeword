178102f221fb06583ff255103b3710f87e6b7dea
Hailan Xu <hailan@ualberta.ca>
2018-05-06 07:15:10 -0600

    brings info to message screen

--- SafeWord/app/src/main/java/com/example/safeword/safeword/MainActivity.java
+++ SafeWord/app/src/main/java/com/example/safeword/safeword/MainActivity.java
@@ -1,11 +1,24 @@
 package com.example.safeword.safeword;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.telephony.gsm.SmsManager;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView setupText;
     private ImageView infoButton;
     private TextView infoText;
+    private String txtString = "smsto:000911,";
+    protected static final String FILENAME = "info.sav";
+    protected ArrayList<String> phoneNumbers = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         infoButton.setOnClickListener(info);
         infoText.setOnClickListener(info);
 
-
+        callButton.setOnClickListener(message);
     }
 
     View.OnClickListener setup = new View.OnClickListener() {
@@ -50,5 +66,40 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
     };
+
+    View.OnClickListener message = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(Intent.ACTION_SENDTO,Uri.parse(getTxtString()));
+            intent.putExtra("sms_body", "This is Elizabeth. I am in danger right now. My location is at: DEVICE LOCATION HERE");
+            startActivity(intent);
+        }
+    };
+
+    public String getTxtString(){
+        loadFromFile();
+        for(int i=0; i<phoneNumbers.size();i++){
+            txtString = txtString + phoneNumbers.get(i) + ",";
+        }
+        return txtString;
+    }
+
+    protected void loadFromFile() {
+
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Gson gson = new Gson();
+
+            Type listType = new TypeToken<ArrayList<String>>(){}.getType();
+
+            phoneNumbers = gson.fromJson(in, listType);
+
+        } catch (FileNotFoundException e) {
+            phoneNumbers = new ArrayList<String>();
+        }
+    }
+
 
 }
